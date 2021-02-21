36f86bf4109727229435330cc3a88a8f0813670f
Smertin Nikita <smertin-nikita@mail.ru>
2021-02-21 22:17:49 +0500

    Added tests

--- test_accounting.py
+++ test_accounting.py
@@ -4,13 +4,31 @@ import accounting
 class TestAccounting:
 
     def setup(self):
-        print("Start test")
+        self.type = 'passport'
+        self.number = '1111 111111'
+        self.name = 'Name Name'
+        self.shelf = '4'
 
-    def teardown(self):
-        print('Stop test')
+    def test_add_shelf(self):
+        assert accounting.add_shelf(self.shelf)
 
     def test_add_document(self):
-        assert accounting.add_document("insurance", "10006", "Аристарх Павлов", '4')
+        assert accounting.add_document(self.type, self.number, self.name, self.shelf)
+
+    def test_get_documents(self):
+        assert accounting.get_documents(self.number)
+
+    def test_print_people(self):
+        assert accounting.print_people(self.number)
+
+    def test_get_all_documents(self):
+        assert accounting.get_all_documents()
+
+    def test_get_shelfs(self):
+        assert accounting.get_shelfs(self.number)
+
+    def test_move_document_to_1_shelf(self):
+        assert accounting.move_document(self.number, '1')
 
     def test_del_document(self):
-        assert accounting.del_documents("10006")
+        assert accounting.del_documents(self.number)
